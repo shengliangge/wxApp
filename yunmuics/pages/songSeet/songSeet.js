84b9c8ee8b74eec8518a233cf09791a3610d3c78
shengliangge <850110183@qq.com>
2020-07-28 19:13:41 +0800

    音乐页面

--- yunmuics/pages/songSeet/songSeet.js
+++ yunmuics/pages/songSeet/songSeet.js
@@ -1,67 +1,34 @@
-// pages/find/find.js
+// pages/songSeet/songSeet.js
 const API = require('../../utils/api')
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    indicatorDots: "true",
-    indicatorActiveColor: "rgb(221,65,54)",
-    indicatorColor: "rgba(0,0,0,.3)",
-    autoplay: "true",
-    interval: "5000",
-    duration: "1000",
-    circular: "true",
-    banners: [],
-    recommendList: [],
-    blocks: [],
     songsheet: []
   },
-  tosongSheet: function () {
-    console.log("点击")
-    wx.navigateTo({
-      url: '../songSeet/songSeet'
-    })
-  },
-  //获取轮播图
-  getBanner: function () {
-    API.getBanner({
+
+  //获取歌单信息
+  getsongsheet: function () {
+    API.getsongsheet({
       type: 2
     }).then(res => {
       if (res.code === 200) { //更加严谨
-        // console.log(res)
+        console.log(res)
         this.setData({
-          banners: res.banners
+          songsheet: res.playlists
         })
+
       }
     })
   },
-
-
-  // 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    // this.getsongsheet()//查看数据使用
-    this.getBanner()
-    wx.request({
-      url: 'https://music.163.com/api/homepage/block/page', //仅为示例，并非真实的接口地址
-      data: {
-      },
-      header: {
-        "Content-Type": "application/json"
-      },
-      //成功回调函数 成功 200
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          blocks: res.data.data.blocks
-        })
-      }
-    })
-
+    this.getsongsheet()
   },
 
   /**
