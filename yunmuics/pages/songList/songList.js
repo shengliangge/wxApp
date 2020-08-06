ad0eec3fcc1c6e11614258d22580b5c34d8bccd3
shengliangge <850110183@qq.com>
2020-08-06 22:08:07 +0800

    test

--- yunmuics/pages/songList/songList.js
+++ yunmuics/pages/songList/songList.js
@@ -1,4 +1,5 @@
 // pages/songlist/songlist.js
+const app = getApp();
 Page({
 
   /**
@@ -29,16 +30,59 @@ Page({
         author: '薛之谦',
         name: '天外来物'
       }
-    ]
+    ],
+    userInfo: {},
+    login_token: '',
+    songs: [],
+    playlist:{}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    //接受其他页面传来的数据
+    let listId=options.listId
+    console.log(listId)
+    wx.request({
+      url: 'http://neteasecloudmusicapi.zhaoboy.com/playlist/detail',
+      data: {
+        "id": listId,
+        "cookie": this.data.login_token
+      },
+      header: {
+        "Content-Type": "application/json",
+        "cookie": this.data.login_token
+      },
+      //成功回调函数 成功 200
+      success: (res) => {
+        console.log(res)
+        console.log("find第二个歌单登陆成功吗？", res.data)
+        this.setData({
+          playlist: res.data.playlist
+        })
+      }
+    })
   },
-
+  //播放音乐
+  playMusic: function (e) {
+    console.log(e)
+    // 获取音乐id
+    let musicId = e.currentTarget.dataset.in.id
+    // 跳转到播放页面
+    wx.navigateTo({
+      url: `../play/play?musicId=${musicId}`,
+      success: function (res) {
+        // success
+      },
+      fail: function () {
+        // fail
+      },
+      complete: function () {
+        // complete
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
