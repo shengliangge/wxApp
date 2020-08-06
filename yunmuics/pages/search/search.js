a135a87ba9834a05bf6071ed61d9aa61fef2e97d
wu-hu-1201 <1871516072@qq.com>
2020-08-06 22:22:01 +0800

    更新

--- yunmuics/pages/search/search.js
+++ yunmuics/pages/search/search.js
@@ -1,4 +1,5 @@
 // pages/search/search.js
+const API = require('../../utils/api')
 const app = getApp();
 Page({
 
@@ -6,6 +7,14 @@ Page({
    * 页面的初始数据
    */
   data: {
+    hotsongs: [],//获取热门搜索
+    inputValue: null,//输入框输入的值
+    history:[], //搜索历史存放数组
+    searchsuggest:[], //搜索建议
+    showView: true,//组件的显示与隐藏
+    showsongresult:true,
+    searchresult:[],//搜索结果
+    searchKey:[],
     hots: [],
     detail: [
       {
@@ -39,21 +48,100 @@ Page({
   onLoad: function (options) {
 
     wx.request({
-    url: 'http://neteasecloudmusicapi.zhaoboy.com/search/hot/detail',
-    data: {
-    },
-    header: {
-      "Content-Type": "application/json"
-    },
-    success: (res) => {
+      url: 'http://neteasecloudmusicapi.zhaoboy.com/search/hot/detail',
+      data: {
+      },
+      header: {
+        "Content-Type": "application/json"
+      },
+      success: (res) => {
+        console.log(res)
+        this.setData({
+          hots: res.data.result.hots
+        })
+      }
+    })
+    // wx.request({
+    //   url: 'http://neteasecloudmusicapi.zhaoboy.com/search/suggest?keywords= 海阔天空',
+    //   data: {
+    //   },
+    //   header: {
+    //     "Content-Type": "application/json"
+    //   },
+    //   success: (res) => {
+    //     console.log(res)
+    //   }
+    // })
+
+  },
+
+ 
+  
+  
+  
+
+ // 搜索建议
+searchSuggest(){
+  API.searchSuggest({ keywords: this.data.searchKey ,type:'mobile'}).then(res=>{
+    if(res.code === 200){
       console.log(res)
       this.setData({
-        hots: res.data.result.hots
+        searchsuggest:res.result.allMatch
       })
     }
   })
+},
+
+
+
+  getsearchKey:function(e){
+    console.log(e.detail.value) //打印出输入框的值
+    let that = this;
+    if(e.detail.cursor != that.data.cursor){ //实时获取输入框的值
+      that.setData({
+        searchKey: e.detail.value
+      })
+    }
+    if(e.value!=""){ //组件的显示与隐藏
+      that.setData({
+        showView: false
+      })
+    } else{
+      that.setData({
+        showView: ""
+      })
+    }
+    if(e.detail.value!=""){ //解决 如果输入框的值为空时，传值给搜索建议，会报错的bug
+      that.searchSuggest();
+    }  
+  },
+  // 实现点击输入框的×把输入的内容清空
+  clearInput:function(res){
+    this.setData({
+      inputValue: ''
+    })
   },
   
+  //实现直接返回返回上一页的功能，退出搜索界面
+  cancel: function () {
+    wx.switchTab({
+      url: ''
+    })
+  },
+
+
+  
+  // 搜索建议
+  // searchSuggest(){
+  //   API.searchSuggest({ keywords: this.data.searchKey ,type:'mobile'}).then(res=>{
+  //     console.log(res);
+  //     if(res.code === 200){
+  //       this.setData({
+  //         searchsuggest:res.result.allMatch
+  //       })
+  //     }
+  //   })
+  // },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
