6473fb4b86b6b62d567cb0cf04f0bdbf994797b8
shengliangge <850110183@qq.com>
2020-08-18 19:34:34 +0800

    更新

--- yunmuics/pages/search/search.js
+++ yunmuics/pages/search/search.js
@@ -194,7 +194,7 @@ searchSuggest(){
   handlePlayAudio: function (event) { //event 对象，自带，点击事件后触发，event有type,target，timeStamp，currentTarget属性
     const musicId = event.currentTarget.dataset.id; //获取到event里面的歌曲id赋值给audioId
     wx.navigateTo({                                 //获取到id带着完整url后跳转到play页面
-      url: `../play/play?id=${musicId}`
+      url: `../play/play?musicId=${musicId}`
     })
   },
 
